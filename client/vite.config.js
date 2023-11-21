8f521a3723ea8cd325361b1fde0364f06bb8aea0
Shubhangam333 <shubhangam27@gmail.com>
2023-11-21 18:45:16 +0530

    updated api endpoint

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://socio-verse.vercel.app",
+        target: "https://socioverseserver-jd1b.onrender.com",
         changeOrigin: true,
       },
     },
